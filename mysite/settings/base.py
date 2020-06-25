3f6465a0c763f2f8eb6bfe36a5165541481ea0af
Anastasia Widyadari <widi.adari@hotmail.com>
2020-06-25 16:39:14 +0100

    base.py removed wagtailmenus

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'flex',
     'streams',
     'site_settings',
-    'wagtailmenus',
     'wagtail.contrib.modeladmin',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
@@ -93,7 +92,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'wagtail.contrib.settings.context_processors.settings',
-                'wagtailmenus.context_processors.wagtailmenus',
 
             ],
         },
